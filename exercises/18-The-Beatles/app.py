0df6d71d36a80f1d80111980c35a417402ce7bc8
CatalinaScheleff <catascheleff@gmail.com>
2023-04-13 17:33:08 +0000

    19

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,13 @@
 # Your code here!!
+def sing():
+    lyrics = ""
+    for i in range (0,11):
+        if (i == 4):
+            lyrics += "whisper words of wisdom, "
+        elif (i == 10):
+            lyrics += "there will be an answer, let it be"
+        else:
+            lyrics += "let it be, "
+    return lyrics
+
+print(sing())
